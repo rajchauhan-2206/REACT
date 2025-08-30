80eb6ea132f0a51fc3698652d584b63404506ae1
rajchauhan-2206 <RAJCHAUHAN2206@GMAIL.COM>
2025-08-30 11:23:13 +0000

    jsx

--- App.js
+++ App.js
@@ -1,40 +1,17 @@
-/**
- * 
- * <div id="parent">
- *  <div id="child">
- *    <h1>i'm h1 tag</h1>
- *    <h2>i'm h2 tag</h2>
- *  <div>
- * </div>
- * 
- * 
- * reactElement(object) => html(Browser understand)
- * 
- */
 
-// const parent = React.createElement(
-//   "div", 
-//   { id: "perent" },
-//    React.createElement(
-//     "div",
-//     { id: "child" },[
-//    React.createElement("h1", {}, "i'm h1 tag"),
-//     React.createElement("h2", {}, "i'm h2 tag"),
-//   ]
+const parent = React.createElement("div",{id: "parent"},[
+  React.createElement("div",{id: "child" },[
+    React.createElement("h1", {},"i'm  raj"),
+    React.createElement("h2", {}, "i'm  raj"),
+  ]),
+  React.createElement("div",{id: "child2"},[
+    React.createElement("h1", {},"i'm web developer"),
+    React.createElement("h2", {},"i'm aslo application devloper"),
+  ]),
+]);
 
-//    )
-//   );
-const group = React.creatElement(
-  "div"
-    { id:"abhi" },
-      React.createElement(
-        "div",
-        { id:"raj" },[
-          React.createElement("h1", {}, "abhi is doctor"),
-          React.creatElement("h2", {}, "raj is soft.eng.")
-        ]
-      )
-);
+//jsx 
+
   //const heading = React.createElement(
         //    "h1",
           //  {id: "heading",xyz:"raj"},  
@@ -42,8 +19,8 @@ const group = React.creatElement(
         // );  creat javascript obj
 
           
-          console.log(group);
+          console.log(parent);//obj
 
             const root = ReactDOM.createRoot(document.getElementById("root"));
 
-          root.render(group); //obj // take this obj creat h1 element for browser understanding put them inside the root
+          root.render(parent); //obj // take this obj creat h1 element for browser understanding put them inside the root
