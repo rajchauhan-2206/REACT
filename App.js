158a038143a0624360d85f754539be84f64baf1c
rajchauhan-2206 <RAJCHAUHAN2206@GMAIL.COM>
2025-09-06 08:56:29 +0000

    hi

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id: "parent"},[
-  React.createElement("div",{id: "child" },[
-    React.createElement("h1", {},"i'm  raj"),
-    React.createElement("h2", {}, "i'm  raj"),
-  ]),
-  React.createElement("div",{id: "child2"},[
-    React.createElement("h1", {},"i'm web developer"),
-    React.createElement("h2", {},"i'm aslo application devloper"),
-  ]),
-]);
+const Title = () =>(
+    <h1 className="raj" tabIndex="5">
+        JAVASCRIPT DEVELOPER
+    </h1>
+);
 
-// //jsx 
+const HeadingComponent = () =>(
+    <div id="container">
+        <Title/>
+    <h1 className="heading">RAJ CHAUHAN</h1>
+    </div>
+);
 
-//   //const heading = React.createElement(
-//         //    "h1",
-//           //  {id: "heading",xyz:"raj"},  
-//             //"hello world from raj chauhan!" 
-//         // );  creat javascript obj
+const root=ReactDOM.createRoot(document.getElementById("root"));
 
-          
-//           console.log(parent);//obj
-
-//             //const root = ReactDOM.createRoot(document.getElementById("root"));
-//                         const root = ReactDOM.createRoot(document.getElementById("header")); //assign root
-
-//           root.render(parent); //obj // take this obj creat h1 element for browser understanding put them inside the root
-          
-// // render method is put an obj on to our page
+root.render(<HeadingComponent/>);
